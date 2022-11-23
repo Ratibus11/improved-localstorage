05bf1c698f4527a1bc8604381b5b399596540bb1
Ratibus11 <49524093+Ratibus11@users.noreply.github.com>
2022-11-23 12:11:41 +0100

    Github wiki-like generated documentation

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,18 +3,21 @@ import * as gulp from "gulp";
 // Gulp modules
 import * as gulpTypescript from "gulp-typescript";
 import * as gulpUglify from "gulp-uglify";
+import * as gulpTypedoc from "gulp-typedoc";
 
 // Utils
-import * as fs from "fs";
+import * as fsExtra from "fs-extra";
 import * as merge2 from "merge2";
 import * as path from "path";
 import * as browserify from "browserify";
 import * as vinylSourceStream from "vinyl-source-stream";
 import * as vinylBuffer from "vinyl-buffer";
+import * as chokidar from "chokidar";
+import * as glob from "glob";
 
 const cwd = __dirname;
 const paths = {
-    clean: ["build"],
+    clean: ["docs", "build"],
     typescript: {
         tsconfig: path.resolve("tsconfig.json"),
         entry: path.resolve("src/main.ts"),
@@ -31,6 +34,14 @@ const paths = {
             name: "app.js",
         },
     },
+    documentation: {
+        main: path.resolve("docs"),
+        version: {
+            path: path.resolve("docs", process.env.npm_package_version!),
+            glob: path.resolve("docs", process.env.npm_package_version!, "**/*.*"),
+            clean: { include: ["**/*"], exclude: ["**/[!README]*.md"] },
+        },
+    },
 };
 
 /**
@@ -41,8 +52,8 @@ function clean(done: gulp.TaskFunctionCallback) {
     paths.clean.forEach((folder) => {
         const folderPath = path.resolve(__dirname, folder);
 
-        if (fs.existsSync(folderPath)) {
-            fs.rmSync(folderPath, { recursive: true });
+        if (fsExtra.existsSync(folderPath)) {
+            fsExtra.rmSync(folderPath, { recursive: true });
         }
     });
 
@@ -79,7 +90,7 @@ function minify(done: gulp.TaskFunctionCallback) {
         .pipe(gulpUglify())
         .pipe(gulp.dest(paths.build.app.path))
         .on("end", () => {
-            fs.rmSync(paths.transpiled.app, { recursive: true });
+            fsExtra.rmSync(paths.transpiled.app, { recursive: true });
             done();
         });
 }
@@ -89,9 +100,102 @@ function minify(done: gulp.TaskFunctionCallback) {
  * @param done Callback function.
  */
 function documentate(done: gulp.TaskFunctionCallback) {
+    // Delete old non-markdown file (and README.md)
+    ["**/*"].forEach((docGlob) => {
+        glob.sync(docGlob, {
+            cwd: paths.documentation.version.path,
+            ignore: ["**/[!README]*.md"],
+            dot: true,
+            absolute: true,
+        }).forEach((filePath) => {
+            if (fsExtra.lstatSync(filePath).isFile()) {
+                fsExtra.rmSync(filePath);
+            }
+        });
+    });
+
+    // Rewrite all markdowns' links
+    glob.sync(paths.documentation.version.glob, {
+        cwd: paths.documentation.version.path,
+        absolute: true,
+    }).forEach((filePath) => {
+        const versionnedName = path.relative(paths.documentation.main, filePath);
+        const newFileName = path.resolve(
+            paths.documentation.version.path,
+            versionnedName.replaceAll("/", "-")
+        );
+
+        var content = fsExtra.readFileSync(filePath).toString();
+
+        content.match(/\[.+?\]\(.+?\)/g)?.forEach((markdownLink) => {
+            const linkPath = markdownLink.match(/\(.*?\)/)![0].slice(1, -1);
+
+            const absolutePath = path.resolve(path.dirname(filePath), linkPath);
+
+            const anchorPosition = absolutePath.lastIndexOf("#");
+            const absoluePathWithoutAnchor = absolutePath.slice(
+                0,
+                anchorPosition === -1 ? undefined : anchorPosition
+            );
+
+            if (fsExtra.existsSync(absoluePathWithoutAnchor)) {
+                const newLinkPath = path
+                    .relative(paths.documentation.main, absolutePath)
+                    .replaceAll("/", "-");
+                const newMarkdownLink = markdownLink.replace(linkPath, newLinkPath);
+
+                content = content.replace(markdownLink, newMarkdownLink);
+            } else if (
+                path.relative(absoluePathWithoutAnchor, paths.documentation.version.path) == ".." &&
+                path.basename(absoluePathWithoutAnchor) === "README.md"
+            ) {
+                const newMarkdownLink = markdownLink.replace(linkPath, "Home.md");
+                content = content.replace(markdownLink, newMarkdownLink);
+            }
+        });
+
+        const newFilePath = path.resolve(
+            paths.documentation.version.path,
+            path.relative(paths.documentation.main, filePath).replaceAll("/", "-")
+        );
+        fsExtra.writeFileSync(newFilePath, content);
+        fsExtra.rmSync(filePath);
+    });
+
+    // Delete all non-markdown files ()
+    glob.sync("**/*", { absolute: true, cwd: paths.documentation.version.path }).forEach(
+        (filePath) => {
+            if (fsExtra.lstatSync(filePath).isDirectory()) {
+                fsExtra.rmSync(filePath, { recursive: true });
+            }
+        }
+    );
+
     done();
+
+    /*return gulp
+        .src(paths.typescript.glob)
+        .pipe(gulpTypedoc({ out: paths.documentation.out, version: true }))
+        .on("end", () => {
+            const watcher = chokidar.watch(paths.documentation.out, { depth: 1 });
+
+            watcher.on("add", () => {
+                watcher.close();
+
+                // FUNCTION
+            });
+        });*/
 }
 
 gulp.task("clean", gulp.series(clean));
 
 gulp.task("build", gulp.series(clean, transpile, minify));
+
+gulp.task("documentate", gulp.series(copy, documentate));
+
+function copy(done: gulp.TaskFunctionCallback) {
+    fsExtra.rmSync("docs", { recursive: true });
+    fsExtra.copySync("docs-save", "docs");
+
+    done();
+}
